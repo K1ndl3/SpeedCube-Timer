6e535e2351b4d173e49b18efe1c4efd7bd6f85fe
K1ndl3 <HUYBUI02@csu.fullerton.edu>
2025-06-08 23:45:18 -0700

    TODO: catch up with doc

--- src/components/NameCard/NameCard.jsx
+++ src/components/NameCard/NameCard.jsx
@@ -3,7 +3,11 @@ import './NameCard.css'
 function NameCard() {
     return (
         <>
-            <h1>SpinChrono</h1>
+            <h1>
+                {'SpinChrono'.split('').map((char, idx) => (
+                    <span key={idx}>{char}</span>
+                ))}
+            </h1>
         </>
     )
 }
--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -2,8 +2,19 @@ import './Timer.css'
 
 function Timer() {
 
+    const timeParts = "00:00:00".split(":");
+
     return(<>
-    <div className="timer-container">asdfasdf</div>
+    <div className="timer-container">
+        <div className="timer-display">
+            {timeParts.map((val, index)  => (
+                <span className='timer-display-digits' key={index}>
+                    {val}
+                    {index < timeParts.length - 1 && <span>:</span>}
+                    </span>
+           ))}
+        </div>
+    </div>
     </>)
 }
 
